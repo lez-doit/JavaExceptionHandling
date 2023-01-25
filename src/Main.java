b346ff85505285fa26bf0fa5e88d631e529040ac
Artem Popov <br4v0.2403@gmail.com>
2023-01-25 11:34:54 +0200

    InoutMismatchException catching added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter a whole number to divide");
+            int x = scanner.nextInt();
+
+            System.out.println("Enter a whole number to divide by");
+            int y = scanner.nextInt();
+
+            int z = x / y;
+            System.out.println("Result: " + z);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("You cant divide by zero!");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Type only numbers!");
+        }
     }
 }
